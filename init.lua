925e5b5b58da38168a207235895071ad66caf82f
Simão Gato <simao.valedegato@gmail.com>
2024-08-10 22:12:46 +0100

    feat: config nvim to support goland

--- init.lua
+++ init.lua
@@ -572,7 +572,17 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              analyses = {
+                unusedparams = true,
+                shadow = true,
+              },
+              staticcheck = true,
+            },
+          },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -613,6 +623,10 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'gopls', -- Go Language Server
+        'goimports', -- Used to format Go code
+        'gofumpt', -- Used to format Go code
+        'golangci-lint', -- Used to lint Go code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
